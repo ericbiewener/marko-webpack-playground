ffe48296d355c7c59a7a21eddf23deedba7ad53a
Eric Biewener <ebiewener@ebay.com>
2024-05-25 10:59:43 -0700

    maybe component is present during SSR

--- src/pages/index/index.ts
+++ src/pages/index/index.ts
@@ -1,5 +1,7 @@
 import template from "./template.marko";
 
 export default (req, res) => {
-  res.marko(template, {});
+  res.marko(template, {
+    showInfoCard: req.query.showInfoCard === "1",
+  });
 };
